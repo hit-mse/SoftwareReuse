136a3e8ed5d0b707b802714f3c57b8a9ed4c4370
Gustav Aaro <gustav.aaro@gmail.com>
2019-03-07 18:34:54 +0800

    Add empty angular app

---
+++ stock-master-front/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class StockMasterFrontPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
